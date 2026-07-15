dd2f1eccc9be29a1d205cd1f12f0aa42d90935fd
mauricio <mauriciobenjamin700@gmail.com>
2026-07-15 15:32:38 -0300

    chore: release v0.130.0

--- tempest_fastapi_sdk/__init__.py
+++ tempest_fastapi_sdk/__init__.py
@@ -407,7 +407,7 @@ from tempest_fastapi_sdk.websockets import (
     make_websocket_router,
 )
 
-__version__: str = "0.129.0"
+__version__: str = "0.130.0"
 
 __all__: list[str] = [
     "BASE_COLUMN_ORDER",
